1d9bb0d0d3a05350d024000abbfca5732b930fde
Winice Hui <winice.hui@gmail.com>
2025-06-16 13:17:45 -0400

    Testing file generate_rss.py, update README and generated XML file

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# 🐦 Twitter RSS Feed Generator for @CUSocialMedia
-This project fetches the most recent tweets from [@CUSocialMedia](https://x.com/CUSocialMedia) using the Twitter API, formats them into an RSS feed, and displays them on our CampusPress Social Media Lab site using the **RSS Just Better** plugin, styled with custom CSS and JavaScript. It is displayed here:  https://socialmedialab.cornell.edu/news/
+# 𝕏 RSS Feed Generator for @CUSocialMedia
+This project fetches the most recent tweets from [@CUSocialMedia](https://x.com/CUSocialMedia) using the X API, formats them into an RSS feed, and displays them on our CampusPress Social Media Lab site using the **RSS Just Better** plugin, styled with custom CSS and JavaScript. It is displayed here:  https://socialmedialab.cornell.edu/news/
 
 ---
 
@@ -7,7 +7,7 @@ This project fetches the most recent tweets from [@CUSocialMedia](https://x.com/
 X's current privacy policies requires users to log in to X to see tweets. Hence, when we embed the @CUSocialMedia X timeline (via [Twitter Publish](https://publish.twitter.com/#)), the displayed timeline says "No Posts to Show", or entirely disappears.
 
 ## Our solution: 
-Use the Twitter API to fetch the most recent tweets, format them into an RSS feed, and display them on our WordPress site using the **RSS Just Better** plugin, styled with custom CSS and JavaScript. It is displayed here:  https://socialmedialab.cornell.edu/news/
+Use the X API to fetch the most recent tweets, format them into an RSS feed, and display them on our WordPress site using the **RSS Just Better** plugin, styled with custom CSS and JavaScript. It is displayed here:  https://socialmedialab.cornell.edu/news/
 - 🔁 A **Python script** [`scripts/generate_rss.py`](scripts/generate_rss.py) retrieves the 10 most recent tweets via the Twitter API.
 - 📄 Tweets are converted into a styled **RSS feed XML file** [https://cornellsml.github.io/sml-tweets-cache/feeds/cusocialmedia_rss.xml](https://cornellsml.github.io/sml-tweets-cache/feeds/cusocialmedia_rss.xml).
 - 🌐 The XML file is **hosted on GitHub Pages**.
--- scripts/generate_rss.py
+++ scripts/generate_rss.py
@@ -122,11 +122,408 @@ def create_rss_feed(tweets, username):
     return ET.ElementTree(rss)
 
 def save_rss(tree, filename):
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
     with open(filename, 'wb') as f:
         tree.write(f, pretty_print=True, xml_declaration=True, encoding='utf-8')
 
 if __name__ == '__main__':
-    tweets = get_user_tweets(USER_ID)
+    # tweets = get_user_tweets(USER_ID)
+    tweets = {
+    "data": [
+        {
+            "text": "🎓🎉 Congratulations, Dr. Zhao! We’re celebrating @Pengfei_Zhao_  on her successful dissertation defense. We’re so proud of you at the SML, and will miss you dearly! https://t.co/yr1mAuBOfd",
+            "entities": {
+                "annotations": [
+                    {
+                        "start": 24,
+                        "end": 27,
+                        "probability": 0.5404,
+                        "type": "Other",
+                        "normalized_text": "Zhao"
+                    },
+                    {
+                        "start": 133,
+                        "end": 135,
+                        "probability": 0.6116,
+                        "type": "Organization",
+                        "normalized_text": "SML"
+                    }
+                ],
+                "mentions": [
+                    {
+                        "start": 48,
+                        "end": 62,
+                        "username": "Pengfei_Zhao_",
+                        "id": "1250120269978451968"
+                    }
+                ],
+                "urls": [
+                    {
+                        "start": 164,
+                        "end": 187,
+                        "url": "https://t.co/yr1mAuBOfd",
+                        "expanded_url": "https://x.com/CUSocialMedia/status/1925546186385322257/photo/1",
+                        "display_url": "pic.x.com/yr1mAuBOfd",
+                        "media_key": "3_1925244185667399680"
+                    },
+                    {
+                        "start": 164,
+                        "end": 187,
+                        "url": "https://t.co/yr1mAuBOfd",
+                        "expanded_url": "https://x.com/CUSocialMedia/status/1925546186385322257/photo/1",
+                        "display_url": "pic.x.com/yr1mAuBOfd",
+                        "media_key": "3_1925244198984294400"
+                    },
+                    {
+                        "start": 164,
+                        "end": 187,
+                        "url": "https://t.co/yr1mAuBOfd",
+                        "expanded_url": "https://x.com/CUSocialMedia/status/1925546186385322257/photo/1",
+                        "display_url": "pic.x.com/yr1mAuBOfd",
+                        "media_key": "3_1925244206727024640"
+                    },
+                    {
+                        "start": 164,
+                        "end": 187,
+                        "url": "https://t.co/yr1mAuBOfd",
+                        "expanded_url": "https://x.com/CUSocialMedia/status/1925546186385322257/photo/1",
+                        "display_url": "pic.x.com/yr1mAuBOfd",
+                        "media_key": "3_1925244217682501632"
+                    }
+                ]
+            },
+            "attachments": {
+                "media_keys": [
+                    "3_1925244185667399680",
+                    "3_1925244198984294400",
+                    "3_1925244206727024640",
+                    "3_1925244217682501632"
+                ]
+            },
+            "edit_history_tweet_ids": [
+                "1925546186385322257"
+            ],
+            "id": "1925546186385322257",
+            "author_id": "132922064",
+            "created_at": "2025-05-22T13:36:00.000Z"
+        },
+        {
+            "text": "🎉 Congratulations to SML Ph.D. student @bae_inhwan on receiving the 2024-25 PCCW Frank H.T. Rhodes Mission Grant! 🌟 Her project focuses on combating online toxicity against women and nonbinary persons with tailored persuasion messages using large language models.",
+            "entities": {
+                "annotations": [
+                    {
+                        "start": 21,
+                        "end": 28,
+                        "probability": 0.4394,
+                        "type": "Organization",
+                        "normalized_text": "SML Ph.D"
+                    },
+                    {
+                        "start": 81,
+                        "end": 111,
+                        "probability": 0.6925,
+                        "type": "Other",
+                        "normalized_text": "Frank H.T. Rhodes Mission Grant"
+                    }
+                ],
+                "mentions": [
+                    {
+                        "start": 39,
+                        "end": 50,
+                        "username": "bae_inhwan",
+                        "id": "1533376662976745472"
+                    }
+                ]
+            },
+            "edit_history_tweet_ids": [
+                "1910701874686144596"
+            ],
+            "id": "1910701874686144596",
+            "author_id": "132922064",
+            "created_at": "2025-04-11T14:30:00.000Z"
+        },
+        {
+            "text": "SML members @bae_inhwan @isabellemcdaph @haesooheatherk and Julia Sebastien had a lovely meeting with the students @btf_bluetree about youth social media safety! It was great to chat, @ParkChanJun_ , thank you for inviting us! #bluetreefoundation",
+            "entities": {
+                "hashtags": [
+                    {
+                        "start": 227,
+                        "end": 246,
+                        "tag": "bluetreefoundation"
+                    }
+                ],
+                "annotations": [
+                    {
+                        "start": 0,
+                        "end": 2,
+                        "probability": 0.5565,
+                        "type": "Organization",
+                        "normalized_text": "SML"
+                    },
+                    {
+                        "start": 60,
+                        "end": 74,
+                        "probability": 0.6971,
+                        "type": "Person",
+                        "normalized_text": "Julia Sebastien"
+                    }
+                ],
+                "mentions": [
+                    {
+                        "start": 12,
+                        "end": 23,
+                        "username": "bae_inhwan",
+                        "id": "1533376662976745472"
+                    },
+                    {
+                        "start": 24,
+                        "end": 39,
+                        "username": "isabellemcdaph",
+                        "id": "253745895"
+                    },
+                    {
+                        "start": 40,
+                        "end": 55,
+                        "username": "haesooheatherk",
+                        "id": "1089674299861012486"
+                    },
+                    {
+                        "start": 115,
+                        "end": 128,
+                        "username": "btf_bluetree",
+                        "id": "237499826"
+                    },
+                    {
+                        "start": 184,
+                        "end": 197,
+                        "username": "ParkChanJun_",
+                        "id": "898837538747502593"
+                    }
+                ]
+            },
+            "edit_history_tweet_ids": [
+                "1896556918576922773"
+            ],
+            "id": "1896556918576922773",
+            "author_id": "132922064",
+            "created_at": "2025-03-03T13:43:00.000Z"
+        },
+        {
+            "text": "We are excited to welcome Han Li, @HanLi_1231  to SML! Han joined our collaborative project on \"Deterring Objectionable Behaviors Online,\" https://t.co/h4T2TGQS5B  She comes to us after her postdoc at the NUS where she worked with Dr. Renwen Zhang (@renwenzhang). Welcome, Han! https://t.co/SRhPmI1Nu4",
+            "entities": {
+                "annotations": [
+                    {
+                        "start": 26,
+                        "end": 31,
+                        "probability": 0.5897,
+                        "type": "Person",
+                        "normalized_text": "Han Li"
+                    },
+                    {
+                        "start": 50,
+                        "end": 52,
+                        "probability": 0.4111,
+                        "type": "Organization",
+                        "normalized_text": "SML"
+                    },
+                    {
+                        "start": 96,
+                        "end": 135,
+                        "probability": 0.7955,
+                        "type": "Other",
+                        "normalized_text": "Deterring Objectionable Behaviors Online"
+                    },
+                    {
+                        "start": 235,
+                        "end": 246,
+                        "probability": 0.6203,
+                        "type": "Other",
+                        "normalized_text": "Renwen Zhang"
+                    },
+                    {
+                        "start": 273,
+                        "end": 275,
+                        "probability": 0.5441,
+                        "type": "Person",
+                        "normalized_text": "Han"
+                    }
+                ],
+                "mentions": [
+                    {
+                        "start": 34,
+                        "end": 45,
+                        "username": "HanLi_1231",
+                        "id": "1580054223420432384"
+                    },
+                    {
+                        "start": 249,
+                        "end": 261,
+                        "username": "renwenzhang",
+                        "id": "2692945104"
+                    }
+                ],
+                "urls": [
+                    {
+                        "start": 139,
+                        "end": 162,
+                        "url": "https://t.co/h4T2TGQS5B",
+                        "expanded_url": "https://socialsciences.cornell.edu/deterring-objectionable-behavior-online",
+                        "display_url": "socialsciences.cornell.edu/deterring-obje…",
+                        "status": 200,
+                        "title": "Deterring Objectionable Behavior Online | Cornell Center for Social Sciences",
+                        "description": "People encounter all kinds of objectionable content online -- misinformation, hate speech, conspiracies, bullying -- things they wish another person wouldn’t say or repeat. They are thus inclined to “object.” But what is the effective way to do so, particularly with so many others watching? What kinds of objectio ns, or other behaviors, have the desired effect of reducing this kind of speech. Our project involves addressing this problem from multiple angles: from observation of the real world to experiment",
+                        "unwound_url": "https://socialsciences.cornell.edu/deterring-objectionable-behavior-online"
+                    },
+                    {
+                        "start": 278,
+                        "end": 301,
+                        "url": "https://t.co/SRhPmI1Nu4",
+                        "expanded_url": "https://x.com/CUSocialMedia/status/1894397692035416123/photo/1",
+                        "display_url": "pic.x.com/SRhPmI1Nu4",
+                        "media_key": "3_1892268894959616000"
+                    }
+                ]
+            },
+            "attachments": {
+                "media_keys": [
+                    "3_1892268894959616000"
+                ]
+            },
+            "edit_history_tweet_ids": [
+                "1894397692035416123"
+            ],
+            "id": "1894397692035416123",
+            "author_id": "132922064",
+            "created_at": "2025-02-25T14:43:00.000Z"
+        },
+        {
+            "text": "Our Ph.D. student @haesooheatherk represented the SML at the UN’s 63rd Session of the Commission for Social Development! Invited by @btf_bluetree, she spoke on what academic research can do to build a safer online future. #BTF https://t.co/lnsgz4VDtM https://t.co/D9xwUNvC4m",
+            "entities": {
+                "hashtags": [
+                    {
+                        "start": 222,
+                        "end": 226,
+                        "tag": "BTF"
+                    }
+                ],
+                "annotations": [
+                    {
+                        "start": 50,
+                        "end": 52,
+                        "probability": 0.4039,
+                        "type": "Organization",
+                        "normalized_text": "SML"
+                    },
+                    {
+                        "start": 61,
+                        "end": 62,
+                        "probability": 0.4984,
+                        "type": "Organization",
+                        "normalized_text": "UN"
+                    }
+                ],
+                "mentions": [
+                    {
+                        "start": 18,
+                        "end": 33,
+                        "username": "haesooheatherk",
+                        "id": "1089674299861012486"
+                    },
+                    {
+                        "start": 132,
+                        "end": 145,
+                        "username": "btf_bluetree",
+                        "id": "237499826"
+                    }
+                ],
+                "urls": [
+                    {
+                        "start": 227,
+                        "end": 250,
+                        "url": "https://t.co/lnsgz4VDtM",
+                        "expanded_url": "https://webtv.un.org/en/asset/k1t/k1tbxblcyi",
+                        "display_url": "webtv.un.org/en/asset/k1t/k…",
+                        "images": [
+                            {
+                                "url": "https://pbs.twimg.com/news_img/1892203411858227200/cXH5ZdRq?format=jpg&name=orig",
+                                "width": 564,
+                                "height": 317
+                            },
+                            {
+                                "url": "https://pbs.twimg.com/news_img/1892203411858227200/cXH5ZdRq?format=jpg&name=150x150",
+                                "width": 150,
+                                "height": 150
+                            }
+                        ],
+                        "status": 200,
+                        "title": "Beyond Access: Youth Online Safety and Digital Equity - CSocD63 Side Event",
+                        "description": "This side event of the 63rd Session of the Commission For Social Development will explore concrete and practical solutions to bridge the digital divide that directly impacts youth safety, well-being, and rights.",
+                        "unwound_url": "https://webtv.un.org/en/asset/k1t/k1tbxblcyi"
+                    },
+                    {
+                        "start": 251,
+                        "end": 274,
+                        "url": "https://t.co/D9xwUNvC4m",
+                        "expanded_url": "https://x.com/CUSocialMedia/status/1892203366509363475/photo/1",
+                        "display_url": "pic.x.com/D9xwUNvC4m",
+                        "media_key": "3_1892203358628282368"
+                    }
+                ]
+            },
+            "attachments": {
+                "media_keys": [
+                    "3_1892203358628282368"
+                ]
+            },
+            "edit_history_tweet_ids": [
+                "1892203366509363475"
+            ],
+            "id": "1892203366509363475",
+            "author_id": "132922064",
+            "created_at": "2025-02-19T13:23:32.000Z"
+        }
+    ],
+    "includes": {
+        "media": [
+            {
+                "url": "https://pbs.twimg.com/media/GrfXYvCXcAAxeJ3.jpg",
+                "media_key": "3_1925244185667399680",
+                "type": "photo"
+            },
+            {
+                "url": "https://pbs.twimg.com/media/GrfXZgpXIAAcpHM.jpg",
+                "media_key": "3_1925244198984294400",
+                "type": "photo"
+            },
+            {
+                "url": "https://pbs.twimg.com/media/GrfXZ9fX0AAy9ai.jpg",
+                "media_key": "3_1925244206727024640",
+                "type": "photo"
+            },
+            {
+                "url": "https://pbs.twimg.com/media/GrfXamTXIAAkvgE.jpg",
+                "media_key": "3_1925244217682501632",
+                "type": "photo"
+            },
+            {
+                "url": "https://pbs.twimg.com/media/GkKwiMeWwAAk1Us.jpg",
+                "media_key": "3_1892268894959616000",
+                "type": "photo"
+            },
+            {
+                "url": "https://pbs.twimg.com/media/GkJ07enXAAAP8w6.jpg",
+                "media_key": "3_1892203358628282368",
+                "type": "photo"
+            }
+        ]
+    },
+    "meta": {
+        "next_token": "7140dibdnow9c7btw4b3nqb5tsik6b7b1w65djxl05jqt",
+        "result_count": 5,
+        "newest_id": "1925546186385322257",
+        "oldest_id": "1892203366509363475"
+    }
+}
     rss_tree = create_rss_feed(tweets, USERNAME)
     save_rss(rss_tree, RSS_FILE)
     print(f"✅ RSS feed saved to {RSS_FILE}")
